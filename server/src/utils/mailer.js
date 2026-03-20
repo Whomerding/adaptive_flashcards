c2b2caac0f02df2c150549cef046c4b076a9d1a2
Whomerding <wendy.homerding@gmail.com>
2026-03-20 09:37:21 -0400

    add credentials to .env for reset password

--- server/src/utils/mailer.js
+++ server/src/utils/mailer.js
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: process.env.SMTP_SECURE === "true",
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
