c362161f45cac48f5c6989c34a3b81d5d80afca9
Whomerding <wendy.homerding@gmail.com>
2026-03-26 13:18:55 -0400

    fix dashboard UI

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -78,7 +78,7 @@ export default function Dashboard() {
   ) : (
     <div>
       <div className="container py-4">
-        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-5 justify-content-center">
           {children.map((child) => (
             <Child
               key={child.id}
